3f3e91fd7b97eb75598e3c909334236d5f420744
P A R A S <bhegadeparas5@gmail.com>
2026-05-20 00:14:32 +0530

    Time: 11 ms (48.80%) | Memory: 46 MB (8.49%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,9 +1,11 @@
-class Solution:
-    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        s1=set(nums1)
-        s2=set(nums2)
-        s1=s1.intersection(s2)
-        if s1==set():
-            return -1
-        else:
-            return min(s1)
+class Solution: 
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int: 
+        s1=set(nums1) 
+        s2=set(nums2) 
+        s1=s1.intersection(s2) 
+        s1=list(s1) 
+        s1.sort() 
+        if len(s1)==0: 
+            return -1 
+        else: 
+            return s1[0]
