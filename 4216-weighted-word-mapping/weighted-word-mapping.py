2dc8f8b4011bd662e11f965239ebe38a6cd40f3b
P A R A S <bhegadeparas5@gmail.com>
2026-06-13 23:43:39 +0530

    Time: 8 ms (64.03%) | Memory: 19.4 MB (8.53%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.py
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -2,11 +2,10 @@ class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
         alpha= "zyxwvutsrqponmlkjihgfedcba"
         wet=''
-        result=0
         for i in words:
+            result=0
             for j in i:
                 result+=weights[ord(j)-ord('a')]
                 result=result%26
             wet+=alpha[result]
-            result=0
         return wet
