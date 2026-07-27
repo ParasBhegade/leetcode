2a937f38c513dc452fb3975963123bc717fa8298
P A R A S <bhegadeparas5@gmail.com>
2026-07-27 19:58:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (57.20%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.py
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.py
@@ -1,13 +1,20 @@
 class Solution:
     def countNumbersWithUniqueDigits(self, n: int) -> int:
-        cnt = 0
-        if(n==0): cnt = 1
-        if(n==1): cnt = 10  
-        if(n==2): cnt = 91
-        if(n==3): cnt = 739
-        if(n==4): cnt = 5275
-        if(n==5): cnt = 32491
-        if(n==6): cnt = 168571
-        if(n==7): cnt = 712891
-        if(n==8): cnt = 2345851
-        return cnt
+        if(n==0): 
+            return 1
+        if(n==1): 
+            return 10  
+        if(n==2): 
+            return 91
+        if(n==3): 
+            return 739
+        if(n==4): 
+            return 5275
+        if(n==5): 
+            return 32491
+        if(n==6): 
+            return 168571
+        if(n==7): 
+            return 712891
+        if(n==8): 
+            return 2345851
