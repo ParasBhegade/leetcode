938436e750114a5c733e404c4d6d2eaac169d41f
P A R A S <bhegadeparas5@gmail.com>
2026-09-03 15:09:54 +0530

    Time: 66 ms (46.98%) | Memory: 23.6 MB (97.83%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,15 +1,19 @@
+import random
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         target=k-1
         def dist(point):
             return point[0]**2+point[1]**2
         def part(points,low,high):
-            pivot=points[low]
+            pivot_index = random.randint(low, high)
+            points[low], points[pivot_index] = points[pivot_index], points[low]
+            pivot = points[low]
+            pivot_dist = dist(pivot)
             i,j=low,high
             while i<j:
-                while i<=high-1 and dist(points[i])<=dist(pivot):
+                while i<=high-1 and dist(points[i])<=pivot_dist:
                     i+=1
-                while j>=low+1 and dist(points[j])>dist(pivot):
+                while j>=low+1 and dist(points[j])>pivot_dist:
                     j-=1
                 if i<j:
                     points[i],points[j]=points[j],points[i]
